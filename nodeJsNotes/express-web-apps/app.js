a986f9902d2eaeca5952140724a09b09ffdaf971
PedroHenrique-git <pedromf34@outlook.com>
2022-03-26 17:47:58 -0300

    feat: more about express

--- nodeJsNotes/express-web-apps/app.js
+++ nodeJsNotes/express-web-apps/app.js
@@ -4,16 +4,29 @@ const greetMiddleware = require('./modules/greet.js');
 const GreetingService = require('./modules/gretting-service');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 const APP_PORT = process.env.PORT || 8080;
 
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(function(req, res, next) {
+    req.user = 'testuser';
+    next();
+});
+
+app.post('/post-data-here', function(req, res, next) {
+    console.log('BODY --> ', req.body);
+});
+
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Test title'});
+    const user = req.user;
+    console.log(user);
+    res.render('index', { title: 'Test title', user });
 });
 
 app.use(cors());
@@ -29,10 +42,49 @@ app.get('/get',  (req, res) => {
 app.use(express.static('public'));
 app.use('/static', express.static('public'));
 
-app.use((req, res, next) => {
-    const err = new Error('Not found');
-    err.status = 404;
-    next(err);
+/*
+    app.use((req, res, next) => {
+        const err = new Error('Not found');
+        err.status = 404;
+        next(err);
+    });
+*/
+
+app.get('/names', function(req, res, next) {
+    if(req.params.name === 'john') {
+        return res.send('VALID NAME');
+    } else {
+        next(new Error('Not valid name'));
+    }
+});
+
+app.use(function(req, res, next) {
+    const afterResponse = () => {
+        req.removeListener('finish', afterResponse);
+        req.removeListener('close', afterResponse);
+        console.log('EVENTO DISPARADO');
+    }
+
+    res.on('finish', afterResponse);
+    res.on('close', afterResponse);
+
+    next();
+});
+
+app.get('/setcookie', function(req, res) {
+    res.cookie('username', 'john doe', {
+        maxAge: 90000,
+        httpOnly: true
+    });
+    return res.send('Cookie has been set');
+});
+
+app.get('/getcookie', function(req, res) {
+    const username = req.cookies['username'];
+    if(username) {
+        return res.send(username);
+    }
+    return res.send('No cookie found');
 });
 
 app.use((err, req, res, next) => {
