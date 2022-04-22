d99cb23482f1e756c6c488280e283dc6eab01283
PedroHenrique-git <pedromf34@outlook.com>
2022-04-21 22:19:44 -0300

    feat: middlewares

--- first-express-project/src/app.js
+++ first-express-project/src/app.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const { friends } = require('./models/friends');
+const crypto = require('crypto');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use((req, __, next) => {
+  console.time();
+
+  console.log(`
+    METHOD: ${req.method}
+    URL: ${req.url}
+    PARAMS: ${JSON.stringify(req.params)}
+  `);
+  next();
+
+  console.timeEnd();
+});
+
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({ message: 'missing friend name' });
+  }
+
+  const newFriend = {
+    id: crypto.randomUUID(),
+    name: req.body.name,
+  };
+
+  friends.push(newFriend);
+
+  res.status(200).json(newFriend);
+});
 
 app.get('/friends', (_, res) => {
   res.status(200).json(friends);
