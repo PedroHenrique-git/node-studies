6cdcf834cb36137693d65cd4e32aa688c71495a7
PedroHenrique-git <pedromf34@outlook.com>
2022-07-24 22:39:28 -0300

    feat: add mongoose

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,12 +1,21 @@
+import dotenv from 'dotenv';
 import http from 'http';
+import mongoose from 'mongoose';
 import { loadPlanetsData } from '../data';
 import app from './app';
 
+dotenv.config();
+
 const PORT = process.env.PORT || 8000;
+const MONGODB_URL = process.env.MONGODB_CONNECTION_STRING;
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => console.log('mongodb connection ready!'));
+mongoose.connection.on('error', (err) => console.error(err));
+
 async function startServer() {
+    await mongoose.connect(MONGODB_URL);
     await loadPlanetsData();
 
     server.listen(PORT, () => {
