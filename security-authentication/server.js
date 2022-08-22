4b6e42a731b5411174c8d5c98e338ec09f3efa81
PedroHenrique-git <pedromf34@outlook.com>
2022-08-21 21:08:51 -0300

    feat: passport

--- security-authentication/server.js
+++ security-authentication/server.js
@@ -3,23 +3,76 @@ const fs = require('fs');
 const helmet = require('helmet');
 const https = require('https');
 const path = require('path');
+const dotenv = require('dotenv');
+const { Strategy } = require('passport-google-oauth20');
+const passport = require('passport');
+const { session } = require('passport');
+
+dotenv.config();
 
 const PORT = 3000;
+
+const config = {
+    CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    CLIENT_SECRET: process.env.GOOGLE_SECRET,
+    callbackURL: '/auth/google/callback' 
+}
+
+const AUTH_OPTIONS = {
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+    callbackURL: config.callbackURL
+}
 
 const serverOptions = {
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem'),
 }
 
+function checkLoggedIn(req, res, next) {
+    const isLoggedIn = true;
+
+    if(!isLoggedIn) {
+        return res.status(401).json({ error: 'You must log in!' });
+    }
+
+    next()
+}
+
+function verifyCallback(accessToke, refreshToken, profile, done) {
+    console.log(profile);
+    done(null, profile)
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['email']
+}));
+
+app.get('/auth/google/callback', 
+    passport.authenticate('google', { 
+        failureRedirect: '/',
+        successRedirect: '/secret',
+        session: false
+    }),
+    () => {
+        console.log('Google called us back!');
+    }
+);
+
+app.get('/auth/logout', (req, res) => {});
 
 app.get('/', (_ ,res) => {
     return res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/secret', (_, res) => {
+app.get('/secret', checkLoggedIn, (_, res) => {
     return res.send('Your personal secret is 401');
 });
 
