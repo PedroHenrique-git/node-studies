6adec0407f761fd9833ae41244d5495905006db7
PedroHenrique-git <pedromf34@outlook.com>
2022-11-29 23:19:19 -0300

    feat: disconnect handle

--- sockets/javascripts/script.js
+++ sockets/javascripts/script.js
@@ -91,6 +91,12 @@ function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
+
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score
+  })
 }
 
 // Adjust Ball Movement
@@ -101,6 +107,12 @@ function ballMove() {
   if (playerMoved) {
     ballX += speedX;
   }
+
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score
+  })
 }
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
@@ -160,9 +172,12 @@ function ballBoundaries() {
 
 // Called Every Frame
 function animate() {
-  ballMove();
+  if(isReferee) {
+    ballMove();
+    ballBoundaries();
+  }
+
   renderCanvas();
-  ballBoundaries();
   window.requestAnimationFrame(animate);
 }
 
@@ -213,5 +228,9 @@ socket.on('startGame', (refereeId) => {
 socket.on('paddleMove', (paddleData) => {
   const opponentPaddleIndex = 1 - paddleIndex
   paddleX[opponentPaddleIndex] = paddleData.xPosition
+})
+
+socket.on('ballMove', (ballData) => {
+  ({ ballX, ballY, score } = ballData)
 })
 
