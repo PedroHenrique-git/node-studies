4a573ec6d8961d3ce53e12644933b83216ad0dc6
PedroHenrique-git <pedromf34@outlook.com>
2022-12-02 20:23:47 -0300

    feat: deno

--- deno/main.ts
+++ deno/main.ts
@@ -1,1 +1,11 @@
+import { desc, run, sh, task } from "https://deno.land/x/drake/mod.ts";
+
 console.log('Hello', Deno.env.get('USERNAME'))
+
+desc("Minimal Drake task");
+task("hello", [], async function () {
+  console.log("Hello from Drake!");
+  await sh("deno run --allow-env main.ts");
+});
+
+run();
